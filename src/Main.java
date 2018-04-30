b209409b34e257790a0efe061eaf4212ba00fe79
Szymon <szymekze@gmail.com>
2018-04-30 17:47:34 +0200

    Próba

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
-public class Main {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Main{
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
     }
 }
