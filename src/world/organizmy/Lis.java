58311c7c4ccacf42c9359ee5b620382649b97dc3
SzymonZebrowski <szymekze@gmail.com>
2018-05-20 10:50:25 +0200

    Almost done
    Abstraction for world unfinished

--- src/world/organizmy/Lis.java
+++ src/world/organizmy/Lis.java
@@ -63,7 +63,68 @@ public class Lis extends Zwierze {
                 idz(x + dx, y + dy);
         }
         else{
-            //hex
+            boolean flu =false, fru=false, fl=false, fr=false, fld=false, frd=false, f;
+            Random rand=new Random();
+            int dx, dy;
+            do{
+                dx=0;
+                dy=0;
+                int pom=rand.nextInt(6);
+                //L
+                if(pom==0){
+                    dx=-1;
+                    fl=true;
+                }
+                //LU
+                else if(pom==1){
+                    if(y%2==0) {dy=-1; dx=-1;}
+                    else{dy=-1; dx=0;}
+                    flu=true;
+                }
+                //RU
+                else if(pom==2) {
+                    if(y%2==0) {dy=-1; dx=0;}
+                    else{dy=-1; dx=1;}
+                    fru=true;
+                }
+                //R
+                else if(pom==3){
+                    dx=1;
+                    fr=true;
+                }
+                //RD
+                else if(pom==4) {
+                    if(y%2==0) {dy=1; dx=0;}
+                    else{dy=1; dx=1;}
+                    frd=true;
+                }
+                //LD
+                else if(pom==5){
+                    if(y%2==0) {dy=1; dx=-1;}
+                    else{dy=1; dx=0;}
+                    fld=true;
+                }
+                f = fl && flu && fld && fr && frd && fru;
+
+                if (mozeIsc(dx, dy)) {
+                    if (!swiat.czyPuste(x + dx, y + dy)) {
+                        if (swiat.zwrocOrganizm(x + dx, y + dy).zwrocSile() <= sila) {
+                            break;
+                        } else continue;
+                    } else break;
+                }
+            }while(!f);
+
+            if (f) {
+                dx = 0;
+                dy = 0;
+            }
+
+            swiat.dodajKomunikat(nazwa + " rusza się z (" + x + "," + y + ") na (" + (x + dx) + "," + (y + dy) + ")");
+            System.out.println(nazwa + " rusza się z (" + x + "," + y + ") na (" + (x + dx) + "," + (y + dy) + ")");
+
+            if (dx != 0 || dy != 0)
+                idz(x + dx, y + dy);
         }
     }
 
--- src/world/organizmy/Zwierze.java
+++ src/world/organizmy/Zwierze.java
@@ -26,18 +26,50 @@ public abstract class Zwierze extends Organizm {
 
         }
         else{
-            //hex
+            int dx=0,  dy=0;
+            Random rand = new Random();
+            do{
+                dx=0;
+                dy=0;
+                int pom=rand.nextInt(6);
+                //L
+                if(pom==0){
+                   dx=-1;
+                }
+                //LU
+                else if(pom==1){
+                    if(y%2==0) {dy=-1; dx=-1;}
+                    else{dy=-1; dx=0;}
+                }
+                //RU
+                else if(pom==2) {
+                    if(y%2==0) {dy=-1; dx=0;}
+                    else{dy=-1; dx=1;}
+                }
+                //R
+                else if(pom==3){
+                    dx=1;
+                }
+                //RD
+                else if(pom==4) {
+                    if(y%2==0) {dy=1; dx=0;}
+                    else{dy=1; dx=1;}
+                }
+                //LD
+                else if(pom==5){
+                    if(y%2==0) {dy=1; dx=-1;}
+                    else{dy=1; dx=0;}
+                }
+            }while(!mozeIsc(dx,dy));
+
+            swiat.dodajKomunikat(nazwa+" rusza się z ("+x+","+y+") na ("+(x+dx)+","+(y+dy)+")");
+            System.out.println(nazwa+" rusza się z ("+x+","+y+") na ("+(x+dx)+","+(y+dy)+")");
+            idz(x+dx, y+dy);
         }
     }
     public boolean mozeIsc(int dx, int dy){
-        if(swiat.zwrocTypSwiata()==TypSwiata.ZWYKLY){
             if(x+dx>=0 && x+dx<swiat.zwrocSzerokosc() && y+dy>=0 && y+dy<swiat.zwrocWysokosc()) return true;
             else return false;
-        }
-        else{
-            //hex
-            return false;
-        }
     }
     public boolean mozeRozmnozyc(int dx, int dy){
         if(mozeIsc(dx,dy)){
@@ -51,18 +83,15 @@ public abstract class Zwierze extends Organizm {
     public void idz(int dx,int dy){
     px=x;
     py=y;
-        if(swiat.zwrocTypSwiata()==TypSwiata.ZWYKLY){
-            if(swiat.czyPuste(dx, dy)){
-                x=dx;
-                y=dy;
-                swiat.zwolnijPole(px,py);
+
+            if(swiat.czyPuste(dx, dy)) {
+                x = dx;
+                y = dy;
+                swiat.zwolnijPole(px, py);
                 swiat.ustawPole(this);
             }
             else kolizja(this, swiat.zwrocOrganizm(dx,dy));
-        }
-        else{
-            //hex
-        }
+
     }
     public void kolizja(Organizm atakujacy, Organizm obronca){
         swiat.dodajKomunikat("Kolizja na ("+obronca.zwrocX()+","+obronca.zwrocY()+")");
@@ -106,7 +135,7 @@ public abstract class Zwierze extends Organizm {
             boolean fu =false, fd=false, fl=false, fr=false, f;
             Random rand=new Random();
             do{
-                int pom=rand.nextInt(4);
+                int pom=rand.nextInt(6);
                 dx=0;
                 dy=0;
                 if(pom==0) {dx=1; fr=true;}
@@ -127,7 +156,58 @@ public abstract class Zwierze extends Organizm {
             }
         }
         else{
-            //hex
+            boolean flu =false, fru=false, fl=false, fr=false, fld=false, frd=false, f;
+            Random rand=new Random();
+            do{
+                    dx=0;
+                    dy=0;
+                    int pom=rand.nextInt(6);
+                    //L
+                    if(pom==0){
+                        dx=-1;
+                        fl=true;
+                    }
+                    //LU
+                    else if(pom==1){
+                        if(y%2==0) {dy=-1; dx=-1;}
+                        else{dy=-1; dx=0;}
+                        flu=true;
+                    }
+                    //RU
+                    else if(pom==2) {
+                        if(y%2==0) {dy=-1; dx=0;}
+                        else{dy=-1; dx=1;}
+                        fru=true;
+                    }
+                    //R
+                    else if(pom==3){
+                        dx=1;
+                        fr=true;
+                    }
+                    //RD
+                    else if(pom==4) {
+                        if(y%2==0) {dy=1; dx=0;}
+                        else{dy=1; dx=1;}
+                        frd=true;
+                    }
+                    //LD
+                    else if(pom==5){
+                        if(y%2==0) {dy=1; dx=-1;}
+                        else{dy=1; dx=0;}
+                        fld=true;
+                    }
+                f = fl && flu && fld && fr && frd && fru;
+
+                if(obronca.mozeRozmnozyc(dx,dy)) break;
+            }while(!f);
+
+            if(f || obronca.zwrocStan()==StanOrganizmu.BORN){
+                swiat.dodajKomunikat("Nie mozna rozmnozyc od "+nazwa+" na ("+obronca.zwrocX()+","+obronca.zwrocY()+")");
+            }
+            else if(obronca.mozeRozmnozyc(dx,dy)){
+                dodaj(obronca.zwrocX()+dx, obronca.zwrocY()+dy);
+                swiat.dodajKomunikat("Nowy organizm typu "+nazwa+" na ("+(obronca.zwrocX()+dx)+","+(obronca.zwrocY()+dy)+")");
+            }
         }
     }
     abstract public void dodaj(int x, int y);
